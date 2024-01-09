0ca43c8eccdf552e6e094a128f5db7f1f9ee671b
RUVNE <dian.galleon@gmail.com>
2024-01-09 09:35:29 +0800

    fix: served over https error

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,22 +1,25 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title inertia>{{ config('app.name', 'Laravel') }}</title>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
 
-    <!-- Scripts -->
-    <script>
-      window.LARAVEL_ASSET_URL = "{{ asset('') }}";
-    </script>
-    @routes
-    @viteReactRefresh
-    @vite('resources/js/app.tsx')
-    @inertiaHead
-  </head>
-  <body class="font-sans antialiased">
-    @inertia
-  </body>
+  <title inertia>{{ config('app.name', 'Laravel') }}</title>
+
+  <!-- Scripts -->
+  <script>
+    window.LARAVEL_ASSET_URL = "{{ asset('') }}";
+  </script>
+  @routes
+  @viteReactRefresh
+  @vite('resources/js/app.tsx')
+  @inertiaHead
+</head>
+
+<body class="font-sans antialiased">
+  @inertia
+</body>
 
 </html>
