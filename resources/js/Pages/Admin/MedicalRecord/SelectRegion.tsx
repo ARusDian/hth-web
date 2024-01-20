f7c4350c76475dba52c185844e0066ddd375dba6
RUVNE <dian.galleon@gmail.com>
2024-01-20 22:55:22 +0800

    feat: add milk teeth regio

--- resources/js/Pages/Admin/MedicalRecord/SelectRegion.tsx
+++ resources/js/Pages/Admin/MedicalRecord/SelectRegion.tsx
@@ -15,28 +15,56 @@ interface Props {
 }
 
 const REGION = [
-    // Top
+    // Adult
     [
-        // Left
+        // Top
         [
-            18, 17, 16, 15, 14, 13, 12, 11
+            // Left
+            [
+                18, 17, 16, 15, 14, 13, 12, 11
+            ],
+            // Right
+            [
+                21, 22, 23, 24, 25, 26, 27, 28
+            ]
         ],
-        // Right
+        // Bottom
         [
-            21, 22, 23, 24, 25, 26, 27, 28
+            // Left
+            [
+                48, 47, 46, 45, 44, 43, 42, 41
+            ],
+            // Right
+            [
+                31, 32, 33, 34, 35, 36, 37, 38
+            ]
         ]
     ],
-    // Bottom
+    // Milk
     [
-        // Left
+        // Top
         [
-            48, 47, 46, 45, 44, 43, 42, 41
+            // Left
+            [
+                55, 54, 53, 52, 51
+            ],
+            // Right
+            [
+                61, 62, 63, 64, 65
+            ]
         ],
-        // Right
+        // Bottom
         [
-            31, 32, 33, 34, 35, 36, 37, 38
+            // Left
+            [
+                85, 84, 83, 82, 81
+            ],
+            // Right
+            [
+                71, 72, 73, 74, 75
+            ]
         ]
-    ]
+    ],
 ];
 
 
@@ -73,6 +101,13 @@ export default function SelectRegion(props: Props) {
 
                         className="h-96 "
                     />
+                    <p className="text-xl border-t-2 border-gray-200">
+                        Gambar Indeks Region Gigi Susu
+                    </p>
+                    <img src="/assets/image/milk-teeth-region.jpg" alt=""
+
+                        className="h-96 mx-auto "
+                    />
                 </div>
                 <form
                     className=" py-5 border-t-2 border-gray-200"
@@ -81,10 +116,13 @@ export default function SelectRegion(props: Props) {
 
                     <InputLabel htmlFor="type">Pilih Region Gigi untuk penyakit <span className="text-black font-semibold">{props.record.disease?.name} {props.sub_disease_record ? ` - ${props.sub_disease_record.sub_disease?.name}` : null}</span></InputLabel>
 
-                    <div className="max-w-7xl mx-auto">
+                    <div className="max-w-7xl mx-auto flex flex-col">
+                        <p className="text-xl font-semibold">
+                            Gigi Dewasa
+                        </p>
                         {
-                            REGION.map((row, i) => (
-                                <div className={`grid grid-cols-2 ${i + 1 < REGION.length ? "border-b-4 border-gray-100" : ""}`} key={i}>
+                            REGION[0].map((row, i) => (
+                                <div className={`grid grid-cols-2 ${i + 1 < REGION[0].length ? "border-b-4 border-gray-100" : ""}`} key={i}>
                                     {
                                         row.map((half, j) => (
                                             <div className={`grid grid-cols-8 gap-2 ${j % 2 == 1 ? "place-content--start pl-2 border-l-2 border-gray-100" : "pr-2 place-content--end border-r-2 border-gray-100"}`} key={
@@ -114,7 +152,41 @@ export default function SelectRegion(props: Props) {
                                 </div>
                             ))
                         }
-
+                        <p className="text-xl font-semibold">
+                            Gigi Susu
+                        </p>
+                        {
+                            REGION[1].map((row, i) => (
+                                <div className={`grid grid-cols-2 ${i + 1 < REGION[1].length ? "border-b-4 border-gray-100" : ""}`} key={i}>
+                                    {
+                                        row.map((half, j) => (
+                                            <div className={`grid grid-cols-5 gap-2 ${j % 2 == 1 ? "place-content--start pl-2 border-l-2 border-gray-100" : "pr-2 place-content--end border-r-2 border-gray-100"}`} key={
+                                                (i * 2) + j
+                                            } >
+                                                {
+                                                    half.map((it, k) => (
+                                                        <CheckBoxButton
+                                                            key={(i * 2) + j + k}
+                                                            label={it.toString()}
+                                                            checked={form.watch('region')?.includes(it)}
+                                                            onChange={(checked) => {
+                                                                console.log(checked, it, k);
+                                                                if (checked) {
+                                                                    form.setValue('region', [...form.watch('region') || [], it]);
+                                                                } else {
+                                                                    console.log(form.watch('region')!.filter((v: number) => v !== it));
+                                                                    form.setValue('region', form.watch('region')!.filter((v: number) => v !== it));
+                                                                }
+                                                            }}
+                                                        />
+                                                    ))
+                                                }
+                                            </div>
+                                        ))
+                                    }
+                                </div>
+                            ))
+                        }
                     </div>
                     <InputError
                         className="mt-2"
